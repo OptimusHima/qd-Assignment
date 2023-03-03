93297df4202836e33262e174e4c413744cfad95d
Himanshu Chaurasia <72163817+OptimusHima@users.noreply.github.com>
2023-03-03 21:57:05 +0530

    Update Leap_year.js

--- Assignment_Day4-5/Leap_year.js
+++ Assignment_Day4-5/Leap_year.js
@@ -1,27 +1,21 @@
-const prompt = require("prompt-sync")();
-
 function isLeapYear(year) {
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-      return true;
-    } else {
-      return false;
-    }
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  } else {
+    return false;
   }
-  
-  function getNextLeapYear(year) {
-    let nextYear = year + 1;
-    while (!isLeapYear(nextYear)) {
-      nextYear++;
-    }
-    return nextYear;
+}
+
+function getNextLeapYear(year) {
+  let nextYear = year + 1;
+  while (!isLeapYear(nextYear)) {
+    nextYear++;
   }
-  
-  // Get input from the user
-  const year = prompt("Please enter a year:");
-  
-  // Calculate the next leap year
-  const nextLeapYear = getNextLeapYear(year);
-  
-  // Print the result
-  
-  console.log("the next leap year, After " + year + " is " + nextLeapYear);
+  return nextYear;
+}
+
+// Get input 
+const year = parseInt(prompt("Please enter a year:"));
+
+const nextLeapYear = getNextLeapYear(year);
+console.log("the next leap year, After " + year + " is " + nextLeapYear);
